2165e98faad2053138c68d93981f3648c4bf6d94
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2021-04-18 23:52:50 +0300

    Refactor sample feed presenter unit test to use kotlin run blocking test and mockito kotlin helper.

--- app/src/test/java/com/sample/app/FeedPresenterTests.kt
+++ app/src/test/java/com/sample/app/FeedPresenterTests.kt
@@ -2,12 +2,22 @@ package com.sample.app
 
 import com.sample.app.data.network.APIClient
 import com.sample.app.data.network.models.response.Launch
+import com.sample.app.data.network.models.response.Links
 import com.sample.app.presentation.feed.FeedInput
 import com.sample.app.presentation.feed.FeedPresenter
 import com.sample.app.presentation.feed.models.FeedItem
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.net.URL
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -16,31 +26,52 @@ import org.mockito.Mockito
  */
 
 class FeedPresenterTests {
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+        testDispatcher.cleanupTestCoroutines()
+    }
 
     @Test
-    fun getPastLaunches_onSuccessfulDataFetch_shouldUpdateUI() {
+    fun getPastLaunches_onSuccessfulDataFetch_shouldUpdateUI() = runBlockingTest {
         // Init mock objects
-        val feedInputMock = Mockito.mock(FeedInput::class.java)
-        val apiServiceMock = Mockito.mock(APIClient::class.java)
+        val feedInputMock = mock<FeedInput>()
+        val apiServiceMock = mock<APIClient>()
         val presenter = FeedPresenter(feedInputMock, apiServiceMock)
 
-        // Init stub object
-        val launchStub = Mockito.mock(Launch::class.java)
+        // Init stub objects
+        val linksStub = mock<Links>()
+        whenever(linksStub.flickr).thenReturn(Links.Flickr(arrayListOf(URL("http://test.com"))))
+
+        val launchStub = mock<Launch>()
+        whenever(launchStub.missionName).thenReturn("test name")
+        whenever(launchStub.links).thenReturn(linksStub)
+
         val fetchDataStub = arrayListOf(launchStub)
-        runBlocking {
-            Mockito.`when`(apiServiceMock.getPastLaunches()).thenReturn(fetchDataStub)
-        }
-        val expectedDataSet = arrayOf(FeedItem(
-            launchStub.missionName,
-            launchStub. details,
-            launchStub.links.flickr.original.firstOrNull()?.toString())
+        whenever(apiServiceMock.getPastLaunches()).thenReturn(fetchDataStub)
+
+        // Init expected result
+        val expectedDataSet = arrayOf(
+                FeedItem(
+                        launchStub.missionName,
+                        launchStub. details,
+                        launchStub.links.flickr.original.firstOrNull()?.toString()
+                )
         )
 
+        // Perform tested action
         presenter.getPastLaunches()
 
-        runBlocking {
-            Mockito.verify(apiServiceMock).getPastLaunches()
-        }
-        Mockito.verify(feedInputMock).updateUI(expectedDataSet)
+        // Verify required methods was called with required parameters
+        verify(apiServiceMock).getPastLaunches()
+        verify(feedInputMock).showProgress()
+        verify(feedInputMock).updateUI(expectedDataSet)
     }
 }
